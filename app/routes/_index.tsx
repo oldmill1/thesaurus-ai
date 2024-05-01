2a1822b5145a0c680ccdf6e22e370e4c77c9ef37
Ankur Taxali <ataxali@gmail.com>
2024-05-01 18:02:10 -0400

    add styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,38 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from '@remix-run/node';
+import styles from '~/styles.css?url';
+import { ActionFunction, json } from '@remix-run/node';
+import { Form } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'Thesaurus' },
+    { name: 'description', content: 'I can help you find similar words.' },
   ];
+};
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const word = formData.get('word');
+
+  console.log(word); // This will log to the server console
+
+  return json({ success: true });
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className='container'>
+      <h1 className='title'>Thesaurus</h1>
+      <p>Enter a word to find synonyms:</p>
+      <Form method='post'>
+        <input
+          name='word'
+          type='text'
+          className='inputField'
+          placeholder='Type a word...'
+        />
+      </Form>
     </div>
   );
 }
